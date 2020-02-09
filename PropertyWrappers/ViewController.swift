68f2f5d39eb9dd9bbb0eaf575039650dd22c3d18
Petro Korienev <soxjke@gmail.com>
2020-02-09 18:38:47 +0200

    Initial Commit

---
+++ PropertyWrappers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PropertyWrappers
+//
+//  Created by Petro Korienev on 2/9/20.
+//  Copyright © 2020 PetroKorienev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
